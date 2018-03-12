2705b0104745ac1c740ce7533a2c54def397ca0b
CYX28 <chanyx17@gmail.com>
2018-03-12 23:41:30 +0800

    Remove unused import

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,7 +1,5 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-
 /**
  * Sorts alphabetically by name and lists all persons in the address book to the user.
  */
@@ -10,6 +8,7 @@ public class SortCommand extends UndoableCommand {
     public static final String COMMAND_WORD = "sort";
     public static final String COMMAND_ALIAS = "sn";
     public static final String MESSAGE_SUCCESS = "Listed all persons sorted alphabetically by name";
+
     @Override
     public CommandResult executeUndoableCommand() {
         return new CommandResult(MESSAGE_SUCCESS);
